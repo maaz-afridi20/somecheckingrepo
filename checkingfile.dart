d2fa4a0e781755d302982e54712afa743ab6955c
maaz-afridi20 <maazafridi.ma47@gmail.com>
2023-12-05 10:18:40 +0500

    some changes in feature branch

--- checkingfile.dart
+++ checkingfile.dart
@@ -2,4 +2,6 @@ void main(List<String> args) {
   print('this is a list of strings');
 
   print('this changes should be in the feature1 branch');
+
+  print('this is something that is different from the feature');
 }
